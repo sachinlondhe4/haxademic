f1e278805ef607756b8678bb75cb282e591df9ed
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-02-14 16:31:22 -0700

    got some things cleaned up with basic setup, drawing styles and camera perspective. now using a ColorGroup scheme.

--- src/com/haxademic/viz/modules/KacheOut.java
+++ src/com/haxademic/viz/modules/KacheOut.java
@@ -1,41 +1,48 @@
 package com.haxademic.viz.modules;
 
 import processing.core.PConstants;
+import toxi.color.TColor;
 
 import com.haxademic.viz.IVizModule;
 import com.haxademic.viz.ModuleBase;
-import com.p5core.cameras.CameraBasic;
+import com.haxademic.viz.elements.GridEQ;
+import com.p5core.cameras.CameraDefault;
 import com.p5core.data.EasingFloat;
-import com.p5core.hardware.midi.MidiWrapper;
+import com.p5core.util.ColorGroup;
+import com.p5core.util.DrawUtil;
 import com.p5core.util.MathUtil;
 
 public class KacheOut
 extends ModuleBase 
 implements IVizModule
 {
+	// dimensions and stuff
 	protected int _stageWidth;
-	protected int _stageHeight;
-	
-	int _numAverages = 32;
+	protected int _stageHeight;	
+	protected int _numAverages = 32;
 
-	int _curMode;
+	// game state
+	protected int _curMode;
+	protected ColorGroup _gameColors;
 
-	// Blocks
-	int _cols = 20;
-	int _rows = 15;
-	Block[][] _blockGrid;
+	// blocks
+	protected int _cols = 10;
+	protected int _rows = 7;
+	protected Block[][] _blockGrid;
 	
 	// should be an array of balls
-	Ball _ball;
-	Paddle _paddle;
+	protected Ball _ball;
+	protected Paddle _paddle;
+	protected GridEQ _background;
 	
 	// game state
 	protected int _gameState;
 	protected final int GAME_READY = 2;
 	protected final int GAME_ON = 3;
 	
-	public KacheOut()
-	{
+	protected final float CAMERA_Z_DEFAULT = 414;
+	
+	public KacheOut() {
 		super();
 		// store and init audio engine
 		initAudio();
@@ -64,20 +71,19 @@ implements IVizModule
 	}
 	
 	public void focus() {
-		p.colorMode( PConstants.RGB, 255, 255, 255, 1 );
-		p.noStroke();
 		newCamera();
 	}
 	
-	public void handleKeyboardInput()
-	{
+	public void handleKeyboardInput() {
 		if ( p.key == 'm' || p.key == 'M' ) {
 			 launchBall();
 		}
 	}
 
 	void newCamera() {
-		_curCamera = new CameraBasic( p, 0, 0, 485 );
+		_curCamera = new CameraDefault( p, 0, 0, 0 );
+		_curCamera.setPosition( _stageWidth/2f, _stageHeight/2f, CAMERA_Z_DEFAULT );
+		_curCamera.setTarget( _stageWidth/2f, _stageHeight/2f, 0 );
 		_curCamera.reset();
 	}
 
@@ -85,21 +91,27 @@ implements IVizModule
 	}
 	
 	public void update() {
+		DrawUtil.resetGlobalProps( p );
+		DrawUtil.setCenter( p );
+
 		p.shininess(1000f); 
 		p.lights();
 		p.background(0);
+//		p.camera();
 		
-//		_curCamera.update();
-		p.rectMode(PConstants.CENTER);
-		
+		_curCamera.update();
+//		p.println(-_stageWidth + p.mouseX*2);
+//		_curCamera.setPosition( _stageWidth/2, _stageHeight/2, -_stageWidth + p.mouseX*2 );
+
 		updateGame();
-//		_curCamera.setPosition( 0, 0, -_stageWidth / 2 + p.mouseX );
-//		p.println(-_stageWidth / 2 + p.mouseX);
 	}
 	
 	// GAME LOGIC --------------------------------------------------------------------------------------
 	
 	public void initGame() {
+		pickNewColors();
+		
+		
 		// create grid
 		float boxW = _stageWidth / _cols;
 		float boxH = _stageHeight / 2 / _rows;
@@ -114,6 +126,9 @@ implements IVizModule
 		}
 		
 		// create game objects
+		_background = new GridEQ( p, toxi, _audioData );
+		_background.updateColorSet( _gameColors );
+
 		_ball = new Ball();
 		_paddle = new Paddle();
 		
@@ -122,6 +137,19 @@ implements IVizModule
 	}
 	
 	protected void updateGame() {
+		// draw bg
+		p.pushMatrix();
+		p.translate( 0, 0, -1000 );
+		_background.update();
+		p.popMatrix();
+		
+		// debug bg
+//		p.pushMatrix();
+//		p.noStroke();
+//		p.fill( 255, 255, 255, 50 );
+//		p.rect( _stageWidth/2, _stageHeight/2, _stageWidth, _stageHeight );
+//		p.popMatrix();
+		
 		// draw the blocks
 		for (int i = 0; i < _cols; i++) {
 			for (int j = 0; j < _rows; j++) {
@@ -163,6 +191,15 @@ implements IVizModule
 		}
 	}
 
+	// Visual fun
+	
+	protected void pickNewColors() {
+		// get themed colors
+		if( _gameColors == null ) {
+			_gameColors = new ColorGroup( ColorGroup.KACHE_OUT );
+		}
+		_gameColors.setRandomGroup();
+	}
 
 
 	// BLOCK OBJECT --------------------------------------------------------------------------------------
@@ -174,6 +211,7 @@ implements IVizModule
 		float r,g,b;
 		int index;
 		protected boolean _active;
+		protected TColor _color;
 		
 		public static final String SIDE_H = "H";
 		public static final String SIDE_V = "V";
@@ -192,6 +230,8 @@ implements IVizModule
 			r = p.random( 0, 80 );
 			g = p.random( 200, 255 );
 			b = p.random( 100, 200 );
+			
+			_color = _gameColors.getRandomColor().copy();
 			
 			_active = true;
 		}
@@ -251,9 +291,11 @@ implements IVizModule
 				p.translate( x, y, 0 );
 				
 				//p.rotateZ( _audioData.getFFT().averages[1] * .01f );
-				p.fill( r, g, b );
+				_color.alpha = p.constrain( 0.5f + zAdd, 0, 1 );
+				p.fill( _color.toARGB() );
 				p.noStroke();
-				p.box( w, h, h + zAdd ); 
+				p.box( w, h, h + zAdd*4 ); 
+//				p.rect( 0, 0, w + zAdd, h + zAdd ); 
 				
 				p.popMatrix();
 			}
@@ -266,12 +308,14 @@ implements IVizModule
 
 		protected float BALL_SIZE = 20;
 		float _x, _y, _speedX, _speedY;
+		protected TColor _color;
 
 		Ball() {
 			// convert speed to use radians
 			_speedX = ( MathUtil.randBoolean( p ) == true ) ? 4 : -4;
 			_x = p.random( 0, _stageWidth );
 			_y = p.random( _stageHeight / 2, _stageHeight );
+			_color = _gameColors.getRandomColor().copy();
 		}
 		
 		public float x() { return _x; }
@@ -306,7 +350,7 @@ implements IVizModule
 			detectWalls();
 			detectPaddle();
 			
-			p.fill( 255 );
+			p.fill( _color.toARGB() );
 			p.pushMatrix();
 			p.translate( _x, _y, 0 );
 			p.sphere( BALL_SIZE );
@@ -351,14 +395,16 @@ implements IVizModule
 		EasingFloat x, y;
 		protected int STAGE_H_PADDING = 40;
 		protected float HEIGHT = 20;
-		protected float _width = 250;
+		protected float _width = 200;
 		protected float _easing = 5f;
 		protected float _center;
+		protected TColor _color;
 
 		Paddle() {
 			_center = ( _stageWidth + _width) / 2;
 			x = new EasingFloat( _center, _easing );
 			y = new EasingFloat( _stageHeight - STAGE_H_PADDING, _easing );
+			_color = _gameColors.getRandomColor().copy();
 		} 
 		
 		public float x() { return x.value(); }
@@ -380,7 +426,8 @@ implements IVizModule
 			p.translate( x.value(), y.value(), 0f );
 			p.rotateX( p.frameCount / 30f );
 			
-			p.fill( 255, 255 * _audioData.getFFT().averages[1], 255 );
+			_color.alpha = 0.5f + _audioData.getFFT().averages[1];
+			p.fill( _color.toARGB() );
 			p.noStroke();
 			p.box( _width, HEIGHT, HEIGHT ); 
 			
