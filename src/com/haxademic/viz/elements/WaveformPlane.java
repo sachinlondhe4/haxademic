e6e3540d016bd4388d9eb499e2870d512b8aadea
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-03-03 00:07:49 -0700

    updated WaveformPlane viz element with base rotation EasingFloat3d object - sticks to 45 degree angles. also connected to public standard inputs for camera, lineMode and reset

--- src/com/haxademic/viz/elements/WaveformPlane.java
+++ src/com/haxademic/viz/elements/WaveformPlane.java
@@ -11,9 +11,11 @@ import com.haxademic.viz.ElementBase;
 import com.haxademic.viz.IVizElement;
 import com.p5core.audio.AudioInputWrapper;
 import com.p5core.audio.WaveformData;
-import com.p5core.data.EasingFloat;
+import com.p5core.data.easing.EasingFloat;
+import com.p5core.data.easing.EasingFloat3d;
 import com.p5core.util.ColorGroup;
 import com.p5core.util.DrawUtil;
+import com.p5core.util.MathUtil;
 
 public class WaveformPlane 
 extends ElementBase 
@@ -25,13 +27,13 @@ implements IVizElement {
 	protected TColor _baseColor;
 	protected ColorGroup _curColors;
 	protected EasingFloat _baseWaveLineSpacing = new EasingFloat( 5f, 5f );
-	protected float _rotation = 0;
-	protected float _rotationTarget = 0;
+	protected EasingFloat3d _rotation = new EasingFloat3d( 0, 0, 0, 5f );
 	
 	protected final int MODE_LINES = 1;
 	protected float _drawMode = MODE_LINES;
 	
 	protected final int NUM_LINES = 50;
+	protected final float _ninteyDeg = p.PI / 2f;
 
 
 	public WaveformPlane( PApplet p, ToxiclibsSupport toxi, AudioInputWrapper audioData ) {
@@ -70,11 +72,18 @@ implements IVizElement {
 		float zDepth = 400;
 		p.translate(0, 0, -zDepth);
 		
-		float ninteyDeg = p.PI / 2f;
-		p.rotateY(ninteyDeg*p.mouseX/100);
-		p.rotateX(ninteyDeg*p.mouseY/100);
-		p.rotateZ(ninteyDeg*p.mouseY/100/4);
+//		float ninteyDeg = p.PI / 2f;
+//		p.rotateY(ninteyDeg*p.mouseX/100);
+//		p.rotateX(ninteyDeg*p.mouseY/100);
+//		p.rotateZ(ninteyDeg*p.mouseY/100/4);
 		
+		// apply base easing rotation
+		_rotation.update();
+		p.rotateY( _rotation.valueX() );
+		p.rotateX( _rotation.valueY() );
+		p.rotateZ( _rotation.valueZ() );
+		
+		// set initial darw color to fade out
 		p.stroke( _baseColor.toARGB() );
 		p.noFill();
 		p.pushMatrix();
@@ -88,13 +97,22 @@ implements IVizElement {
 			for(int i=0; i < NUM_LINES; i++) {
 				// _curColors.getColorFromIndex(i % 1).toARGB()
 				p.stroke( lineColor.toARGB() );
-
+				
+				// set waveform history on drawing object
 				_wave.setWaveform( _waveformDataHistory.get(NUM_LINES - i - 1) );
+				
+				// set stroke width and color
+				float strokeWidth = 5f * (float)(NUM_LINES - i)/(float)NUM_LINES;
+				_strokeWidth = strokeWidth;
+				
+				float alpha = ((float)NUM_LINES - (float)i)/(float)NUM_LINES;
+				lineColor.alpha = ( alpha >= 0 ) ? alpha : 0;
+
 				
 				p.pushMatrix();
 				
 				p.translate(0, -curSpacing, 0);
-				p.rotateX(ninteyDeg);
+				p.rotateX(_ninteyDeg);
 				_wave.setDrawProps(_strokeWidth, p.width/2 + p.width * (i+1)/20, 20f * (NUM_LINES - i)/NUM_LINES);
 				_wave.update();
 				
@@ -102,7 +120,7 @@ implements IVizElement {
 				p.pushMatrix();
 				
 				p.translate(0, curSpacing * 2, 0);
-				p.rotateX(ninteyDeg);
+				p.rotateX(_ninteyDeg);
 				_wave.setDrawProps(_strokeWidth, p.width/2 + p.width * (i+1)/20, 20f * (NUM_LINES - i)/NUM_LINES);
 				_wave.update();
 				
@@ -110,11 +128,10 @@ implements IVizElement {
 				
 				// increment distance from center
 				curSpacing += _baseWaveLineSpacing.value();
-				float strokeWidth = 5f * (float)(NUM_LINES - i)/(float)NUM_LINES;
-				_strokeWidth = strokeWidth;
 				
-				float alpha = (float)(NUM_LINES - i)/(float)NUM_LINES;
-				lineColor.alpha = ( alpha >= 0 ) ? alpha : 0;
+				
+				// draw mesh lines
+				
 			}
 		}
 		
@@ -126,10 +143,22 @@ implements IVizElement {
 		p.popMatrix();
 	}
 	
+	public void updateCamera() {
+		// random 45 degree angles
+		_rotation.setTargetX( _ninteyDeg/2f * MathUtil.randRange( 0, 8 ) );
+		_rotation.setTargetY( _ninteyDeg/2f * MathUtil.randRange( 0, 8 ) );
+		_rotation.setTargetZ( _ninteyDeg/2f * MathUtil.randRange( 0, 8 ) );
+	}
+	
+	public void updateLineMode() {
+		_baseWaveLineSpacing.setTarget( p.random( p.height/300f, p.height/40f ) );
+	}
+	
 	public void reset() {
-		_baseWaveLineSpacing.setTarget( p.random( p.height/300f, p.height/15f ) );
 //		_drawMode = ( p.random( 0f, 4 ) > 3 ) ? 0 : 1;
 //		_drawMode = ( MathUtil.randBoolean( p ) == true ) ? 0 : 1;
+		updateLineMode();
+		updateCamera();
 	}
 	
 	public void dispose() {
