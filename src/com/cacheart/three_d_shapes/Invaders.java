8780bf2139e239cc0af4ae54c38b05916950020b
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-03-03 04:31:42 -0700

    added Invaders test sketch, with new Meshes class to generate nice toxiclibs meshes of different shapes.

---
+++ src/com/cacheart/three_d_shapes/Invaders.java
@@ -0,0 +1,63 @@
+package com.cacheart.three_d_shapes;
+
+import krister.Ess.AudioInput;
+import processing.core.PApplet;
+import processing.core.PConstants;
+import toxi.color.TColor;
+import toxi.geom.mesh.WETriangleMesh;
+
+import com.p5core.audio.AudioInputWrapper;
+import com.p5core.draw.shapes.DrawMesh;
+import com.p5core.draw.shapes.Meshes;
+
+public class Invaders
+	extends PApplet
+{
+	protected WETriangleMesh _invaderMesh_01;
+	protected AudioInputWrapper _audioInput;
+	protected TColor _stroke, _fill;
+
+	public void setup () {
+		// set up stage and drawing properties
+		size( 800, 800, "hipstersinc.P5Sunflow" );
+//		size( 800, 800, PConstants.OPENGL );				//size(screen.width,screen.height,P3D);
+		frameRate( 30 );
+		colorMode( PConstants.RGB, 255, 255, 255, 255 );
+		background( 0 );
+		shininess(1000); 
+		lights();
+		
+		
+		
+		
+		_invaderMesh_01 = Meshes.invader1();
+		_invaderMesh_01.scale( 120 );
+		
+		// set up audio input
+		_audioInput = new AudioInputWrapper( this, false );
+		_audioInput.setNumAverages( 3 );
+		_audioInput.setDampening(.4f);
+		
+		_stroke = new TColor( TColor.WHITE );
+		_fill = new TColor( TColor.WHITE );
+	}
+
+	public void draw() {
+		if(frameCount < 2) return;
+		background( 0 );
+		
+		translate( width/2, height/2, 0 );
+		rotateX(this.PI/8f);
+		rotateY(0.5f + frameCount/100f);
+		
+		
+		DrawMesh.drawMeshWithAudio( (PApplet)this, _invaderMesh_01, _audioInput, 3f, false, _fill, _stroke, 0.25f );
+	}
+	
+	// PApp-level listener for audio input data ------------------------ 
+	public void audioInputData( AudioInput theInput ) {
+		_audioInput.getFFT().getSpectrum(theInput);
+		_audioInput.detector.detect(theInput);
+	}
+
+}
---
+++ src/com/p5core/draw/shapes/Meshes.java
@@ -0,0 +1,73 @@
+package com.p5core.draw.shapes;
+
+import toxi.geom.AABB;
+import toxi.geom.Vec3D;
+import toxi.geom.mesh.Mesh3D;
+import toxi.geom.mesh.WETriangleMesh;
+
+
+public class Meshes {
+	
+	public static WETriangleMesh invader1() {
+		// setup / objects
+		float size = 0.5f;
+		AABB boxMesh = new AABB( size );
+		WETriangleMesh mesh = new WETriangleMesh();
+		
+		// add blocks by row
+		addBoxAtCoordinateToMesh( -1, -4,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  1, -4,  0, boxMesh, mesh );
+
+		addBoxAtCoordinateToMesh( -2, -3,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  1, -3,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh( -1, -3,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  2, -3,  0, boxMesh, mesh );
+
+		addBoxAtCoordinateToMesh( -3, -2,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh( -2, -2,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh( -1, -2,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  1, -2,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  2, -2,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  3, -2,  0, boxMesh, mesh );
+
+		addBoxAtCoordinateToMesh( -4, -1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh( -3, -1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh( -1, -1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  1, -1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  3, -1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  4, -1,  0, boxMesh, mesh );
+
+		addBoxAtCoordinateToMesh( -4,  1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh( -3,  1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh( -2,  1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh( -1,  1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  1,  1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  2,  1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  3,  1,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  4,  1,  0, boxMesh, mesh );
+
+		addBoxAtCoordinateToMesh( -2,  2,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  2,  2,  0, boxMesh, mesh );
+
+		addBoxAtCoordinateToMesh( -3,  3,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh( -1,  3,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  1,  3,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  3,  3,  0, boxMesh, mesh );
+
+		addBoxAtCoordinateToMesh( -4,  4,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh( -2,  4,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  2,  4,  0, boxMesh, mesh );
+		addBoxAtCoordinateToMesh(  4,  4,  0, boxMesh, mesh );
+
+		return mesh;
+	} 
+	
+	public static void addBoxAtCoordinateToMesh( float x, float y, float z, AABB box, WETriangleMesh mesh ) {
+		x = ( x < 0 ) ? x + 0.5f : x - 0.5f;
+		y = ( y < 0 ) ? y + 0.5f : y - 0.5f;
+		z = ( z < 0 ) ? z + 0.5f : z - 0.5f;
+		box.set( x, y, z ); 
+		mesh.addMesh( box.toMesh() );
+		
+	}
+}
