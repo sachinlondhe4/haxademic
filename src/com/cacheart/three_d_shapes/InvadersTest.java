6d4be8ea5ed884fccbb8693646cd174f84a6ece6
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-03-16 00:45:37 -0600

    moved Invaders sketch class to InvadersTest

--- src/com/cacheart/three_d_shapes/InvadersTest.java
+++ src/com/cacheart/three_d_shapes/InvadersTest.java
@@ -10,10 +10,10 @@ import com.p5core.audio.AudioInputWrapper;
 import com.p5core.draw.shapes.Meshes;
 import com.p5core.draw.util.DrawMesh;
 
-public class Invaders
+public class InvadersTest
 	extends PApplet
 {
-	protected WETriangleMesh _invaderMesh_01, _invaderMesh_01_alt;
+	protected WETriangleMesh _invaderMesh_01, _invaderMesh_01_alt, _invaderMesh_02, _invaderMesh_02_alt, _invaderMesh_03, _invaderMesh_03_alt;
 	protected AudioInputWrapper _audioInput;
 	protected TColor _stroke, _fill;
 
@@ -35,6 +35,16 @@ public class Invaders
 		_invaderMesh_01.scale( 70 );
 		_invaderMesh_01_alt.scale( 70 );
 		
+		_invaderMesh_02 = Meshes.invader2( 1 );
+		_invaderMesh_02_alt = Meshes.invader2( 2 );
+		_invaderMesh_02.scale( 70 );
+		_invaderMesh_02_alt.scale( 70 );
+
+		_invaderMesh_03 = Meshes.invader3( 1 );
+		_invaderMesh_03_alt = Meshes.invader3( 2 );
+		_invaderMesh_03.scale( 70 );
+		_invaderMesh_03_alt.scale( 70 );
+
 		// set up audio input
 		_audioInput = new AudioInputWrapper( this, false );
 		_audioInput.setNumAverages( 3 );
@@ -48,13 +58,24 @@ public class Invaders
 		if(frameCount < 2) return;
 		background( 0 );
 		
-		translate( width/2, height/2, 0 );
+		translate( width/2, height/2, -400 );
 		rotateX(this.PI/8f);
-		rotateY(0.5f + frameCount/100f);
+//		rotateY(0.5f + frameCount/100f);
+		rotateY((float)mouseX/100f);
 		
-		WETriangleMesh curMesh = ( this.round( this.frameCount / 30f ) % 2 == 0 ) ? _invaderMesh_01 : _invaderMesh_01_alt;
-		DrawMesh.drawMeshWithAudio( (PApplet)this, curMesh, _audioInput, 3f, false, _fill, _stroke, 0.25f );
-	}
+		WETriangleMesh mesh2 = ( this.round( this.frameCount / 30f ) % 2 == 0 ) ? _invaderMesh_02 : _invaderMesh_02_alt;
+		DrawMesh.drawMeshWithAudio( (PApplet)this, mesh2, _audioInput, 3f, false, _fill, _stroke, 0.25f );
+
+		translate(-800f, 0, 0);
+		
+		WETriangleMesh mesh3 = ( this.round( this.frameCount / 30f ) % 2 == 0 ) ? _invaderMesh_03 : _invaderMesh_03_alt;
+		DrawMesh.drawMeshWithAudio( (PApplet)this, mesh3, _audioInput, 3f, false, _fill, _stroke, 0.25f );
+		
+		translate(1600f, 0, 0);
+		
+		WETriangleMesh mesh1 = ( this.round( this.frameCount / 30f ) % 2 == 0 ) ? _invaderMesh_01 : _invaderMesh_01_alt;
+		DrawMesh.drawMeshWithAudio( (PApplet)this, mesh1, _audioInput, 3f, false, _fill, _stroke, 0.25f );
+}
 	
 	// PApp-level listener for audio input data ------------------------ 
 	public void audioInputData( AudioInput theInput ) {
