4a38871faeeeb8b47bbd485bee582894f73c59f3
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-03-25 04:48:27 -0600

    add static method to draw triangles by looping through an OBJModel

--- src/com/p5core/draw/util/DrawMesh.java
+++ src/com/p5core/draw/util/DrawMesh.java
@@ -4,11 +4,15 @@ import java.util.Iterator;
 
 import processing.core.PApplet;
 import processing.core.PConstants;
+import processing.core.PVector;
+import saito.objloader.OBJModel;
 import toxi.color.TColor;
 import toxi.geom.Matrix4x4;
+import toxi.geom.Triangle3D;
 import toxi.geom.Vec3D;
 import toxi.geom.mesh.Face;
 import toxi.geom.mesh.WETriangleMesh;
+import toxi.processing.ToxiclibsSupport;
 
 import com.p5core.audio.AudioInputWrapper;
 
@@ -85,4 +89,22 @@ public class DrawMesh {
 		}
 	}
 
+	public static void drawObjModel( PApplet p, ToxiclibsSupport toxi, OBJModel model ) {
+		// loop through and set vertices
+		Triangle3D tri;
+		
+		
+		// loop through model's vertices
+		for( int i = 0; i < model.getFaceCount(); i++ ) {
+			// get vertex
+			PVector[] facePoints = model.getFaceVertices( i );
+		
+			tri = new Triangle3D( 
+					new Vec3D( facePoints[0].x, facePoints[0].y, facePoints[0].z ), 
+					new Vec3D( facePoints[1].x, facePoints[1].y, facePoints[1].z ), 
+					new Vec3D( facePoints[2].x, facePoints[2].y, facePoints[2].z )
+				);
+			toxi.triangle( tri );
+		}		
+	}
 }
