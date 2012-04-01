dc5222ac6c4d705d45c7d62635f5f834cd9907b6
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-04-01 17:02:18 -0600

    improved memory usage debug output

--- src/com/p5core/util/DebugUtil.java
+++ src/com/p5core/util/DebugUtil.java
@@ -8,11 +8,12 @@ public class DebugUtil {
 		long allocatedMemory = runtime.totalMemory();  
 		long freeMemory = runtime.freeMemory();  
 		   
-		System.out.println("free memory: " + freeMemory / 1024);  
-		System.out.println("allocated memory: " + allocatedMemory / 1024);  
-		System.out.println("max memory: " + maxMemory /1024);  
-		System.out.println("total free memory: " + (freeMemory + (maxMemory - allocatedMemory)) / 1024);
-
+		System.out.println("-- MEMORY USAGE -----------------");  
+		System.out.println("-- free memory: " + freeMemory / 1024);  
+		System.out.println("-- allocated memory: " + allocatedMemory / 1024);  
+		System.out.println("-- max memory: " + maxMemory /1024);  
+		System.out.println("-- total free memory: " + (freeMemory + (maxMemory - allocatedMemory)) / 1024);
+		System.out.println("---------------------------------");  
 	}
 
 }
