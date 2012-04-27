b50cd0f5cdaab3bcd38ea9311a488715ed043adc
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-04-26 23:22:27 -0600

    refactor and test new models

--- src/com/haxademic/sketch/three_d/MeshLoaderDemo.java
+++ src/com/haxademic/sketch/three_d/MeshLoaderDemo.java
@@ -27,7 +27,7 @@ extends PApplet
 	Renderer _render;
 	
 //	OBJModel _model;
-	MeshPool _objPool;
+	MeshPool _meshPool;
 	WETriangleMesh _mesh;
 //	float _rot;
 	EasingFloat3d _rot;
@@ -62,39 +62,53 @@ extends PApplet
 		_rot = new EasingFloat3d( 0, 0, 0, 10f );
 		
 		
-		_objPool = new MeshPool( p );
+		_meshPool = new MeshPool( p );
 		
 		// 3d text with different options
 		if( RG.initialized() == false ) RG.init( p );
-		RFont font = new RFont( "../data/fonts/HelloDenverDisplay-Regular.ttf", 200, RFont.CENTER);
-		_objPool.addMesh( "HAI", MeshUtil.mesh2dFromTextFont( p, null, "../data/fonts/bitlow.ttf", 200, "HAI", -1, 2, 1f ), 1 );
-		WETriangleMesh helloTextMesh = MeshUtil.mesh2dFromTextFont( p, font, null, -1, "HELLO", -1, 3, 1f );
-		_objPool.addMesh( "HELLO", helloTextMesh, 1 );
-		_objPool.addMesh( "HELLO_3D", MeshUtil.getExtrudedMesh( helloTextMesh, 20 ), 1 );
+		RFont helloDenverFont = new RFont( "../data/fonts/HelloDenverDisplay-Regular.ttf", 200, RFont.CENTER);
+		RFont bitLowFont = new RFont( "../data/fonts/bitlow.ttf", 200, RFont.CENTER);
+		_meshPool.addMesh( "HAI", MeshUtil.mesh2dFromTextFont( p, null, "../data/fonts/bitlow.ttf", 200, "HAI", -1, 2, 1f ), 1 );
+		WETriangleMesh helloTextMesh = MeshUtil.mesh2dFromTextFont( p, helloDenverFont, null, -1, "HELLO", -1, 3, 1f );
+		_meshPool.addMesh( "HELLO", helloTextMesh, 1 );
+		_meshPool.addMesh( "HELLO_3D", MeshUtil.getExtrudedMesh( helloTextMesh, 20 ), 1 );
+		
+		_meshPool.addMesh( "PRESENTS_TEXT", MeshUtil.mesh2dFromTextFont( p, bitLowFont, null, 200, "MODE SET", -1, 2, 0.4f ), 1 );
+
+//		_meshPool.addMesh( "DESIGN_BY", MeshUtil.mesh2dFromTextFont( p, bitLowFont, null, 200, "DESIGN BY:", -1, 2, 0.3f ), 1 );
+//		_meshPool.addMesh( "JON_DESIGN", MeshUtil.mesh2dFromTextFont( p, bitLowFont, null, 200, "JON TRAISTER", -1, 2, 0.4f ), 1 );
+//		_meshPool.addMesh( "RYAN_DESIGN", MeshUtil.mesh2dFromTextFont( p, bitLowFont, null, 200, "RYAN POLICKY", -1, 2, 0.4f ), 1 );
+
+		//		_meshPool.addMesh( "COUNTDOWN_TEXT_1", MeshUtil.getExtrudedMesh( MeshUtil.mesh2dFromTextFont( p, bitLowFont, null, 200, "1", -1, 2, 3f ), 4 ), 1 );
+//		_meshPool.addMesh( "COUNTDOWN_TEXT_2", MeshUtil.getExtrudedMesh( MeshUtil.mesh2dFromTextFont( p, bitLowFont, null, 200, "2", -1, 2, 3f ), 4 ), 1 );
+//		_meshPool.addMesh( "COUNTDOWN_TEXT_3", MeshUtil.getExtrudedMesh( MeshUtil.mesh2dFromTextFont( p, bitLowFont, null, 200, "3", -1, 2, 3f ), 4 ), 1 );
 		
 		// .svg vectors
 		WETriangleMesh cacheSVG = MeshUtil.meshFromSVG( p, "../data/svg/cacheflowe-logo.svg", -1, 10, 1f );
-		_objPool.addMesh( "CACHE", cacheSVG, 0.5f );
-		_objPool.addMesh( "CACHE_EXTRUDE", MeshUtil.getExtrudedMesh( cacheSVG, 20 ), 1 );
-		_objPool.addMesh( "DIAMOND_2D", MeshUtil.getExtrudedMesh( MeshUtil.meshFromSVG( p, "../data/svg/diamond.svg", -1, 3, 0.5f ), 20 ), 1 );
-		_objPool.addMesh( "BIKE_COMMUTER", MeshUtil.meshFromSVG( p, "../data/svg/bike-commuter.svg", -1, 7, 0.5f ), 1 );
+		_meshPool.addMesh( "CACHE", cacheSVG, 0.5f );
+		_meshPool.addMesh( "CACHE_EXTRUDE", MeshUtil.getExtrudedMesh( cacheSVG, 20 ), 1 );
+		_meshPool.addMesh( "DIAMOND_2D", MeshUtil.getExtrudedMesh( MeshUtil.meshFromSVG( p, "../data/svg/diamond.svg", -1, 3, 0.5f ), 20 ), 1 );
+		_meshPool.addMesh( "CACHEFLOWE_LOGOTYPE", MeshUtil.getExtrudedMesh( MeshUtil.meshFromSVG( p, "../data/svg/cacheflowe-logotype.svg", -1, 6, 0.7f ), 4 ), 1 );
+		_meshPool.addMesh( "MODE_SET_LOGOTYPE", MeshUtil.getExtrudedMesh( MeshUtil.meshFromSVG( p, "../data/svg/modeset-logotype.svg", -1, 6, 0.7f ), 4 ), 1 );
 		
-		// img models
-		_objPool.addMesh( "KACHEOUT", MeshUtil.meshFromImg( p, "../data/images/kacheout/kacheout.gif", 1f ), 20f );
-		_objPool.addMesh( "MUSIC_NOTE", MeshUtil.meshFromImg( p, "../data/images/music.gif", 1f ), 10f );
-		_objPool.addMesh( "UFO", MeshUtil.meshFromImg( p, "../data/images/kacheout/invader-01.gif", 1f ), 30f );
-		
-		// .obj models
-		_objPool.addMesh( "POINTER", MeshUtil.meshFromOBJ( p, "../data/models/pointer_cursor_2_hollow.obj", 1f ), 1.5f );
-		_objPool.addMesh( "DIAMOND", MeshUtil.meshFromOBJ( p, "../data/models/diamond.obj", 1f ), 1.2f );
-		_objPool.addMesh( "INVADER", MeshUtil.meshFromOBJ( p, "../data/models/invader.obj", 1f ), 45 );
-		_objPool.addMesh( "LEGO_MAN", MeshUtil.meshFromOBJ( p, "../data/models/lego-man.obj", 1f ), 30 );
-		_objPool.addMesh( "DISCOVERY", MeshUtil.meshFromOBJ( p, "../data/models/the-discovery-multiplied-seied.obj", 1f ), 900 );
-		_objPool.addMesh( "SOCCER_BALL", MeshUtil.meshFromOBJ( p, "../data/models/soccer_ball.obj", 1f ), 100 );
-		_objPool.addMesh( "TOPSECRET", MeshUtil.meshFromOBJ( p, "../data/models/topsecret-seied.obj", 1f ), 400 );
-		_objPool.addMesh( "MODE_SET", MeshUtil.meshFromOBJ( p, "../data/models/mode-set.obj", 1f ), 150 );
-		_objPool.addMesh( "SPIROGRAPH", MeshUtil.meshFromOBJ( p, "../data/models/spirograph-seied.obj", 1f ), 150 );
-		_objPool.addMesh( "CACHEFLOWE", MeshUtil.meshFromOBJ( p, "../data/models/cacheflowe-3d.obj", 1f ), 150 );
+//		_meshPool.addMesh( "BIKE_COMMUTER", MeshUtil.meshFromSVG( p, "../data/svg/bike-commuter.svg", -1, 7, 0.5f ), 1 );
+//		
+//		// img models
+//		_meshPool.addMesh( "KACHEOUT", MeshUtil.meshFromImg( p, "../data/images/kacheout/kacheout.gif", 1f ), 20f );
+//		_meshPool.addMesh( "MUSIC_NOTE", MeshUtil.meshFromImg( p, "../data/images/music.gif", 1f ), 10f );
+//		_meshPool.addMesh( "UFO", MeshUtil.meshFromImg( p, "../data/images/kacheout/invader-01.gif", 1f ), 30f );
+//		
+//		// .obj models
+//		_meshPool.addMesh( "POINTER", MeshUtil.meshFromOBJ( p, "../data/models/pointer_cursor_2_hollow.obj", 1f ), 1.5f );
+//		_meshPool.addMesh( "DIAMOND", MeshUtil.meshFromOBJ( p, "../data/models/diamond.obj", 1f ), 1.2f );
+//		_meshPool.addMesh( "INVADER", MeshUtil.meshFromOBJ( p, "../data/models/invader.obj", 1f ), 45 );
+//		_meshPool.addMesh( "LEGO_MAN", MeshUtil.meshFromOBJ( p, "../data/models/lego-man.obj", 1f ), 30 );
+//		_meshPool.addMesh( "DISCOVERY", MeshUtil.meshFromOBJ( p, "../data/models/the-discovery-multiplied-seied.obj", 1f ), 900 );
+//		_meshPool.addMesh( "SOCCER_BALL", MeshUtil.meshFromOBJ( p, "../data/models/soccer_ball.obj", 1f ), 100 );
+//		_meshPool.addMesh( "TOPSECRET", MeshUtil.meshFromOBJ( p, "../data/models/topsecret-seied.obj", 1f ), 400 );
+//		_meshPool.addMesh( "MODE_SET", MeshUtil.meshFromOBJ( p, "../data/models/mode-set.obj", 1f ), 150 );
+//		_meshPool.addMesh( "SPIROGRAPH", MeshUtil.meshFromOBJ( p, "../data/models/spirograph-seied.obj", 1f ), 150 );
+//		_meshPool.addMesh( "CACHEFLOWE", MeshUtil.meshFromOBJ( p, "../data/models/cacheflowe-3d.obj", 1f ), 150 );
 
 //		_objPool.loadObj( "SUBMISH_HORIZ", 		200, 	"./models/submish-rotated.obj" );
 //		_objPool.loadObj( "SHUTTLE", 			30, 	"./models/Space Shuttle.obj" );
@@ -104,8 +118,8 @@ extends PApplet
 //		_objPool.loadObj( "BANANA", 			0.5f, 	"./models/banana.obj" );
 		
 		
-		_modelIds = _objPool.getIds();
-		_mesh = _objPool.getMesh( _modelIds.get( 0 ) );
+		_modelIds = _meshPool.getIds();
+		_mesh = _meshPool.getMesh( _modelIds.get( 0 ) );
 
 //		ThreeDeeUtil.SmoothToxiMesh( p, _mesh, 2 );
 		
@@ -118,7 +132,7 @@ extends PApplet
 		if( key == ' ' ) {
 			_meshIndex++;
 			if( _meshIndex >= _modelIds.size() ) _meshIndex = 0;
-			_mesh = _objPool.getMesh( _modelIds.get( _meshIndex ) );
+			_mesh = _meshPool.getMesh( _modelIds.get( _meshIndex ) );
 		}
 		if( key == 'w' ) {
 			_wireFrame = !_wireFrame;
