c794234054078b77b59deda7f9440260ba92fa3c
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-05-12 14:09:21 -0600

    add padding to date components for better cross-system file output ordering

--- src/com/haxademic/core/util/SystemUtil.java
+++ src/com/haxademic/core/util/SystemUtil.java
@@ -2,18 +2,21 @@ package com.haxademic.core.util;
 
 import processing.core.PApplet;
 
+import com.haxademic.app.P;
+
 public class SystemUtil {
-	
+
 	public static String getTimestamp( PApplet p ) {
-		return  String.valueOf( p.year() ) + "-" + 
-				String.valueOf( p.month() ) + "-" + 
-				String.valueOf( p.day() ) + "-" + 
-				String.valueOf( p.hour() ) + "-" + 
-				String.valueOf( p.minute() ) + "-" + 
-				String.valueOf( p.second() );
+		// use P.nf to pad date components to 2 digits for more consistent ordering across systems
+		return  String.valueOf( P.year() ) + "-" + 
+				P.nf( P.month(), 2 ) + "-" + 
+				P.nf( P.day(), 2 ) + "-" + 
+				P.nf( P.hour(), 2 ) + "-" + 
+				P.nf( P.minute(), 2 ) + "-" + 
+				P.nf( P.second(), 2 );
 	}
-	
+
 	public static String getTimestampFine( PApplet p ) {
-		return SystemUtil.getTimestamp(p) + "-" + p.nf(p.frameCount, 8); 
+		return SystemUtil.getTimestamp(p) + "-" + P.nf( p.frameCount, 8 ); 
 	}
 }
