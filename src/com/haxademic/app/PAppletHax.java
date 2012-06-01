2c222eb231cc16be66bde57b88115f8bd9b28446
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-05-31 22:11:40 -0600

    prevent stuck key for master audio input gain. we really need to fix the midi/keyboard/osc input situation...

--- src/com/haxademic/app/PAppletHax.java
+++ src/com/haxademic/app/PAppletHax.java
@@ -235,9 +235,16 @@ extends PApplet
 	
 	protected void handleInput( boolean isMidi ) {
 //		p.println("YOU MUST OVERRIDE KEYPRESSED");
-		// audio gain
-		if ( p.key == '.' || _midi.midiPadIsOn( MidiWrapper.PAD_14 ) == 1 ) _audioInput.gainUp(); 
-		if ( p.key == ',' || _midi.midiPadIsOn( MidiWrapper.PAD_13 ) == 1 ) _audioInput.gainDown(); 
+		if( isMidi == true ) {
+			
+		} else {
+			P.println("p.key = "+p.key);
+			// audio gain
+//			if ( p.key == '.' || _midi.midiPadIsOn( MidiWrapper.PAD_14 ) == 1 ) _audioInput.gainUp(); 
+//			if ( p.key == ',' || _midi.midiPadIsOn( MidiWrapper.PAD_13 ) == 1 ) _audioInput.gainDown(); 
+			if ( p.key == '.' ) _audioInput.gainUp(); 
+			if ( p.key == ',' ) _audioInput.gainDown(); 
+		}
 
 	}
 	
