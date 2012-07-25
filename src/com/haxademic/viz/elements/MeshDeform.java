a84482acd3155ecf81dc07962d8924dd6ead2247
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-07-25 00:22:23 -0600

    Swap models & fix model array cycling

--- src/com/haxademic/viz/elements/MeshDeform.java
+++ src/com/haxademic/viz/elements/MeshDeform.java
@@ -46,25 +46,25 @@ implements IVizElement {
 		float scaleMult = 1.0f;
 		_meshPool = new MeshPool( p );
 		_meshPoolDeformed = new MeshPool( p );
-		_meshPool.addMesh( "MUSIC_NOTE", MeshUtil.meshFromImg( p, "../data/images/music.gif", 1f ), 40f * scaleMult );
+//		_meshPool.addMesh( "MUSIC_NOTE", MeshUtil.meshFromImg( p, "../data/images/music.gif", 1f ), 40f * scaleMult );
 		
 //		_meshPool.addMesh( "DIAMOND", MeshUtil.meshFromOBJ( p, "../data/models/diamond.obj", 1f ), 1.2f * scaleMult );
-		_meshPool.addMesh( "INVADER", MeshUtil.meshFromOBJ( p, "../data/models/invader.obj", 1f ), 45 * scaleMult );
+//		_meshPool.addMesh( "INVADER", MeshUtil.meshFromOBJ( p, "../data/models/invader.obj", 1f ), 45 * scaleMult );
 //		_meshPool.addMesh( "LEGO_MAN", MeshUtil.meshFromOBJ( p, "../data/models/lego-man.obj", 1f ), 30 * scaleMult );
 		_meshPool.addMesh( "DISCOVERY", MeshUtil.meshFromOBJ( p, "../data/models/the-discovery-multiplied-seied.obj", 1f ), 900 * scaleMult );
 		_meshPool.addMesh( "TOPSECRET", MeshUtil.meshFromOBJ( p, "../data/models/topsecret-seied.obj", 1f ), 400 * scaleMult );
 		_meshPool.addMesh( "MODE_SET", MeshUtil.meshFromOBJ( p, "../data/models/mode-set.obj", 1f ), 250 * scaleMult );
-		_meshPool.addMesh( "CACHEFLOWE", MeshUtil.meshFromOBJ( p, "../data/models/cacheflowe-3d.obj", 1f ), 150 * scaleMult );
+//		_meshPool.addMesh( "CACHEFLOWE", MeshUtil.meshFromOBJ( p, "../data/models/cacheflowe-3d.obj", 1f ), 150 * scaleMult );
 				
 //		_meshPool.addMesh( "CDW_LOGO", MeshUtil.meshFromSVG( p, "../data/svg/create-denver-logo.svg", -1, 2, 0.8f ), 1 );
-		_meshPool.addMesh( "DIAMOND_2D", MeshUtil.getExtrudedMesh( MeshUtil.meshFromSVG( p, "../data/svg/diamond.svg", -1, 3, 0.5f ), 20 ), 1 );
+//		_meshPool.addMesh( "DIAMOND_2D", MeshUtil.getExtrudedMesh( MeshUtil.meshFromSVG( p, "../data/svg/diamond.svg", -1, 3, 0.5f ), 20 ), 1 );
 
 		WETriangleMesh sphere = new WETriangleMesh();
 		sphere.addMesh( (new Sphere(200)).toMesh( 15 ) );
 		_meshPool.addMesh( "SPHERE", sphere, 1f * scaleMult );
 		
 		// copy models into deformed model pool
-		for( int i=0; i < _meshPool.size() - 1; i++ ) {
+		for( int i=0; i < _meshPool.size(); i++ ) {
 			String id = _meshPool.getIds().get( i );
 			_meshPoolDeformed.addMesh( id, _meshPool.getMesh( id ).copy(), 1f );
 		}
@@ -130,7 +130,7 @@ implements IVizElement {
 
 	public void selectNewModel() {
 		_meshIndex++;
-		if( _meshIndex >= _meshPool.size() - 1 ) _meshIndex = 0;
+		if( _meshIndex >= _meshPool.size() ) _meshIndex = 0;
 		_curMesh = _meshPool.getMesh( _meshPool.getIds().get( _meshIndex ) );
 		_curMeshDeformed = _meshPoolDeformed.getMesh( _meshPoolDeformed.getIds().get( _meshIndex ) );
 	}
