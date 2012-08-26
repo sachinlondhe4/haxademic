67bfac7f77fcb87ed37349279b91fd79bb2e3995
Justin Gitlin <cacheflowe@cacheflowe.com>
2012-08-26 14:52:21 -0600

    Prepend absolute path to image saving

--- src/com/haxademic/app/kacheout/media/PhotoBooth.java
+++ src/com/haxademic/app/kacheout/media/PhotoBooth.java
@@ -4,6 +4,7 @@ import processing.core.PImage;
 
 import com.haxademic.app.P;
 import com.haxademic.app.PAppletHax;
+import com.haxademic.core.util.FileUtil;
 import com.haxademic.core.util.ImageUtil;
 import com.haxademic.core.util.ScreenUtil;
 import com.haxademic.core.util.SystemUtil;
@@ -13,11 +14,14 @@ public class PhotoBooth {
 		// save screenshot and open it back up
 		String screenshotFile = ScreenUtil.screenshotToJPG( p, "bin/output/kacheout/kacheout-" );
 		PImage screenshot = p.loadImage( screenshotFile );
-
-		// save kinect
-		p.kinectWrapper.getVideoImage().save( "bin/output/kacheout/kacheout-" + SystemUtil.getTimestampFine( p ) + "-rgb.png" );
+		
 
 		if( p.kinectWrapper.isActive() ) {
+			// save normal kinect image
+			String projectPath = FileUtil.getProjectAbsolutePath();
+			p.kinectWrapper.getVideoImage().save( projectPath + "/bin/output/kacheout/kacheout-" + SystemUtil.getTimestampFine( p ) + "-rgb.png" );
+
+			// create composite image
 			float screenToOutputWidthRatio = 640f / (float)stageWidth;
 			int screenShotHeight = Math.round( stageHeight * screenToOutputWidthRatio );
 			PImage img = p.createImage(640, 480 + screenShotHeight, P.RGB);
@@ -25,7 +29,7 @@ public class PhotoBooth {
 			// paste 2 images together and save
 			img.copy( ImageUtil.getReversePImage( p.kinectWrapper.getVideoImage() ), 0, 0, 640, 480, 0, 0, 640, 480 );
 			img.copy( screenshot, 0, 0, stageWidth, stageHeight, 0, 481, 640, screenShotHeight );
-			img.save( "bin/output/kacheout/kacheout-" + SystemUtil.getTimestampFine( p ) + "-comp.png" );
+			img.save( projectPath + "/bin/output/kacheout/kacheout-" + SystemUtil.getTimestampFine( p ) + "-comp.png" );
 		}
 
 		// clean up screenshot
