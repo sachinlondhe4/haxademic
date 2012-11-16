79621c8c90e64476157ac776679a08579f567eff
cacheflowe <cacheflowe@cacheflowe.com>
2012-11-16 15:45:12 -0700

    Update image particle explosions - add confetti

--- src/com/haxademic/sketch/particle/ConfettiExplosion.java
+++ src/com/haxademic/sketch/particle/ConfettiExplosion.java
@@ -3,27 +3,32 @@ package com.haxademic.sketch.particle;
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PImage;
+import toxi.color.TColor;
+import toxi.math.noise.PerlinNoise;
 
 import com.haxademic.core.render.Renderer;
+import com.haxademic.core.util.DrawUtil;
 
 /**
  * 
  * @author justin
  *
  */
-public class ImageParticleExplosion
+public class ConfettiExplosion
 	extends PApplet
 {
 	// global vars
 	protected int _fps = 30;
-	protected ImageParticle[] _particles;
-	protected int _numParticles = 60;
+	protected ConfettiParticle[] _particles;
+	protected int _numParticles = 300;
 //	protected Renderer _render;
+	PApplet p;
 
 	public void setup ()
 	{
+		p = this;
 		// set up stage and drawing properties
-		size( 1024, 768, OPENGL );
+		size( 800, 600, OPENGL );
 		frameRate( _fps );
 		colorMode( PConstants.RGB, 255, 255, 255, 255 );
 		background( 0 );
@@ -40,9 +45,9 @@ public class ImageParticleExplosion
 		imageMode( PConstants.CENTER );
 		
 		// create particles
-		_particles = new ImageParticle[_numParticles];
+		_particles = new ConfettiParticle[_numParticles];
 		for( int i = 0; i < _numParticles; i++ ) {
-			_particles[i] = new ImageParticle( width/2, height/4, image );
+			_particles[i] = new ConfettiParticle( width/2, height/2, image );
 		}
 		
 		// set up renderer
@@ -52,6 +57,7 @@ public class ImageParticleExplosion
 
 	public void draw() 
 	{
+		background(0);
 		// update particles
 		for (int i = 0; i < _numParticles; i++) 
 		{
@@ -77,7 +83,7 @@ public class ImageParticleExplosion
 	}
 	
 	// A Cell object
-	class ImageParticle 
+	class ConfettiParticle 
 	{
 		protected PImage _image;
 		protected float _xOrig;
@@ -86,6 +92,7 @@ public class ImageParticleExplosion
 		protected float _y;
 		protected float _xVelocity;
 		protected float _yVelocity;
+		protected float _maxYVelocity;
 		protected float _rotVelocity;
 		protected float _w;
 		protected float _h;
@@ -94,12 +101,21 @@ public class ImageParticleExplosion
 		protected float _tintG;
 		protected float _tintB;
 		protected float _tintAlpha;
+		protected PerlinNoise _perlin;
+		protected float _perlinOffset;
+		protected float _perlinWind;
+		
+		protected TColor _color;
+		protected TColor BLUE_DARK = TColor.newHex("0f568b");
+		protected TColor BLUE_LIGHT = TColor.newHex("9cb3c7");
+		protected TColor GREY_MEDIUM = TColor.newHex("d4d4d4");
 
-		public ImageParticle( float x, float y, PImage img ) 
+		public ConfettiParticle( float x, float y, PImage img ) 
 		{
 			_image = img;
 			_xOrig = x;
 			_yOrig = y;
+			_perlin = new PerlinNoise();
 			
 			reset();
 		} 
@@ -108,14 +124,25 @@ public class ImageParticleExplosion
 		{
 			_x = _xOrig;
 			_y = _yOrig;
-			float randSize = random( 20, 120 );
+			float randSize = random( 6, 24 );
 			_w = randSize;
 			_h = randSize;
 			_rot = 0;
+			_perlinOffset = random( 0f, 1000f );
 			
-			_xVelocity = random( -30, 30 );
-			_yVelocity = random( -20, 20 );
+			_xVelocity = random( -15, 15 );
+			_yVelocity = random( -50, -30 );
+			_maxYVelocity = random( 3f, 5f );
 			_rotVelocity = random( -.2f, .2f );
+			
+			int randColor = round( random( 0, 2 ) );
+			if( randColor == 0 ) {
+				_color = BLUE_DARK;
+			} else if( randColor == 1 ) {
+				_color = BLUE_LIGHT;
+			} else {
+				_color = GREY_MEDIUM;
+			}
 			
 			_tintR = random( .4f, 1 );
 			_tintG = random( .3f, .6f );
@@ -128,25 +155,40 @@ public class ImageParticleExplosion
 			// draw
 			pushMatrix();
 			
-			tint( _tintR * 255f, _tintG * 255f, _tintB * 255f, _tintAlpha * 255f ); 
+			tint( _tintR * 255f, _tintG * 255f, _tintB * 255f, _tintAlpha * 255f );
+			
+			if( _yVelocity > 0 )
+				_perlinWind = -0.5f + _perlin.noise( _perlinOffset + ( frameCount / 80f ) );
+			else
+				_perlinWind = 0;
 			
 			//rotate( _rot );
+			DrawUtil.setDrawCenter(p);
 			translate( _x, _y );
-			fill(255,255);
-			image( _image, 0, 0, _w, _h );
+			fill( _color.toARGB() );
+			rotateX(_rot);
+			rotateY(_rot);
+			rotateZ(_rot);
+//			image( _image, 0, 0, _w, _h );
 //			blend( _image, (int)_x, (int)_y, (int)_w, (int)_h, (int)_x, (int)_y, (int)_w, (int)_h, ADD);
-//			rect(0,0,10,10);
+			rect(0,0,_w,_w*0.5f);
 			popMatrix();
 			
 			// increment values
-			_x += _xVelocity;
+			_x += _xVelocity + _perlinWind * 10f;
 			_y += _yVelocity;
-			_xVelocity *= .98;
-			_yVelocity += .5;
+			if( _yVelocity < 0 ) {
+				_xVelocity *= .98;
+				_yVelocity += 1.8f;
+			} else {
+				if( _yVelocity < _maxYVelocity ) _yVelocity += .1f;
+				_xVelocity *= .9;
+			}
 			_rot += _rotVelocity;
-			_w *= .94;
-			_h *= .94;
-			_tintAlpha *= .95;
+			
+//			_w *= .99;
+//			_h *= .99;
+//			_tintAlpha *= .999;
 		}
 	}
 	
