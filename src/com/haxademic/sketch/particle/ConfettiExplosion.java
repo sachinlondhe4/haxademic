c2fad29923b75a8fb699e6c985e26fd12a5ff4ae
cacheflowe <cacheflowe@cacheflowe.com>
2012-11-16 20:50:18 -0700

    Clean up and encapsulate confetti code for reuse

--- src/com/haxademic/sketch/particle/ConfettiExplosion.java
+++ src/com/haxademic/sketch/particle/ConfettiExplosion.java
@@ -2,11 +2,9 @@ package com.haxademic.sketch.particle;
 
 import processing.core.PApplet;
 import processing.core.PConstants;
-import processing.core.PImage;
 import toxi.color.TColor;
 import toxi.math.noise.PerlinNoise;
 
-import com.haxademic.core.render.Renderer;
 import com.haxademic.core.util.DrawUtil;
 
 /**
@@ -18,10 +16,8 @@ public class ConfettiExplosion
 	extends PApplet
 {
 	// global vars
-	protected int _fps = 30;
-	protected ConfettiParticle[] _particles;
-	protected int _numParticles = 300;
-//	protected Renderer _render;
+	protected int _fps = 40;
+	protected ConfettiParticles _confetti;
 	PApplet p;
 
 	public void setup ()
@@ -32,75 +28,86 @@ public class ConfettiExplosion
 		frameRate( _fps );
 		colorMode( PConstants.RGB, 255, 255, 255, 255 );
 		background( 0 );
-//		noSmooth();
-//		shininess(1000); 
-//		lights();
+		
+		shininess(1000); 
+		lights();
+		
 		smooth();
 		noStroke();
 		
-		
-		// set up image
-		PImage image;
-		image = loadImage("../data/images/middle-finger-trans.png");
-		imageMode( PConstants.CENTER );
-		
-		// create particles
-		_particles = new ConfettiParticle[_numParticles];
-		for( int i = 0; i < _numParticles; i++ ) {
-			_particles[i] = new ConfettiParticle( width/2, height/2, image );
-		}
-		
-		// set up renderer
-//		_render = new Renderer( this, _fps, Renderer.OUTPUT_TYPE_IMAGE, "bin/output/" );
-//		_render.startRenderer();
+		_confetti = new ConfettiParticles( 300 );
+		_confetti.explode();
 	}
 
 	public void draw() 
 	{
 		background(0);
-		// update particles
-		for (int i = 0; i < _numParticles; i++) 
-		{
-			_particles[i].update();
-		}
+		_confetti.update();
 	}
-	
-	/**
-	 * Key handling for rendering functions - stopping and saving an image
-	 */
-	public void keyPressed()
-	{
-//		if( key == 'p' ) _render.renderFrame();
-	}  
-	
+		
 	public void mouseClicked()
 	{
-		// update particles
-		for (int i = 0; i < _numParticles; i++) 
-		{
-			_particles[i].reset();
+		_confetti.explode();
+	}
+	
+	class ConfettiParticles {
+		protected ConfettiParticle[] _particles;
+		protected int _numParticles;
+		protected boolean _active;
+
+		public ConfettiParticles( int numParticles ) {
+			_numParticles = numParticles;
+			_active = false;
+			
+			// create particles
+			_particles = new ConfettiParticle[_numParticles];
+			for( int i = 0; i < _numParticles; i++ ) {
+				_particles[i] = new ConfettiParticle( width/2, height/2 );
+			}
+		}
+		
+		public void update() {
+			if( _active == true ) {
+				boolean stillActive = false;
+				for (int i = 0; i < _numParticles; i++) {
+					if( _particles[i].update() == true ) {
+						stillActive = true;
+					}
+				}
+				if( stillActive == false ) _active = false;
+			}
+		}
+		
+		public void explode() {
+			_active = true;
+			reset();
+			for (int i = 0; i < _numParticles; i++) {
+				_particles[i].setActive();
+			}
+		}
+		
+		public void reset() {
+			for (int i = 0; i < _numParticles; i++) {
+				_particles[i].reset();
+			}
 		}
 	}
 	
 	// A Cell object
 	class ConfettiParticle 
 	{
-		protected PImage _image;
-		protected float _xOrig;
-		protected float _yOrig;
+		protected boolean _isActive;
+		protected float _originX;
+		protected float _originY;
 		protected float _x;
 		protected float _y;
-		protected float _xVelocity;
-		protected float _yVelocity;
-		protected float _maxYVelocity;
-		protected float _rotVelocity;
+		protected float _speedX;
+		protected float _speedY;
+		protected float _maxSpeedY;
+		protected float _speedRot;
+		protected float _rot;
 		protected float _w;
 		protected float _h;
-		protected float _rot;
-		protected float _tintR;
-		protected float _tintG;
-		protected float _tintB;
-		protected float _tintAlpha;
 		protected PerlinNoise _perlin;
 		protected float _perlinOffset;
 		protected float _perlinWind;
@@ -110,31 +117,38 @@ public class ConfettiExplosion
 		protected TColor BLUE_LIGHT = TColor.newHex("9cb3c7");
 		protected TColor GREY_MEDIUM = TColor.newHex("d4d4d4");
 
-		public ConfettiParticle( float x, float y, PImage img ) 
+		public ConfettiParticle( float x, float y ) 
 		{
-			_image = img;
-			_xOrig = x;
-			_yOrig = y;
+			_isActive = false;
+			_originX = x;
+			_originY = y;
 			_perlin = new PerlinNoise();
 			
 			reset();
 		} 
 		
-		public void reset()
-		{
-			_x = _xOrig;
-			_y = _yOrig;
-			float randSize = random( 6, 24 );
-			_w = randSize;
-			_h = randSize;
+		public void setActive() {
+			_isActive = true;
+		}
+		
+		public void reset() {
+			_isActive = false;
+			
+			// start position and set size
+			_x = _originX;
+			_y = _originY;
+			_w = random( 6, 24 );
+			_h = _w / 2f;
+			
+			// motion vars
+			_speedX = random( -15, 15 );
+			_speedY = random( -50, -30 );
+			_maxSpeedY = random( 3f, 5f );
+			_speedRot = random( -.15f, .15f );
 			_rot = 0;
 			_perlinOffset = random( 0f, 1000f );
 			
-			_xVelocity = random( -15, 15 );
-			_yVelocity = random( -50, -30 );
-			_maxYVelocity = random( 3f, 5f );
-			_rotVelocity = random( -.2f, .2f );
-			
+			// random color
 			int randColor = round( random( 0, 2 ) );
 			if( randColor == 0 ) {
 				_color = BLUE_DARK;
@@ -143,52 +157,49 @@ public class ConfettiExplosion
 			} else {
 				_color = GREY_MEDIUM;
 			}
-			
-			_tintR = random( .4f, 1 );
-			_tintG = random( .3f, .6f );
-			_tintB = random( .4f, .9f );
-			_tintAlpha = random( .8f, .99f );
 		}
 
-		public void update() 
-		{
-			// draw
+		public boolean update() {
+			if( _isActive == false ) return false;
+				
+			// draw setup
 			pushMatrix();
-			
-			tint( _tintR * 255f, _tintG * 255f, _tintB * 255f, _tintAlpha * 255f );
-			
-			if( _yVelocity > 0 )
+			DrawUtil.setDrawCenter(p);
+						
+			// apply perlin wind on the way down
+			if( _speedY > 0 )
 				_perlinWind = -0.5f + _perlin.noise( _perlinOffset + ( frameCount / 80f ) );
 			else
 				_perlinWind = 0;
 			
-			//rotate( _rot );
-			DrawUtil.setDrawCenter(p);
+			// move and rotate
 			translate( _x, _y );
+			rotateX( _rot );
+			rotateY( _rot );
+			rotateZ( _rot );
+
+			// color and draw confetti
 			fill( _color.toARGB() );
-			rotateX(_rot);
-			rotateY(_rot);
-			rotateZ(_rot);
-//			image( _image, 0, 0, _w, _h );
-//			blend( _image, (int)_x, (int)_y, (int)_w, (int)_h, (int)_x, (int)_y, (int)_w, (int)_h, ADD);
 			rect(0,0,_w,_w*0.5f);
 			popMatrix();
 			
-			// increment values
-			_x += _xVelocity + _perlinWind * 10f;
-			_y += _yVelocity;
-			if( _yVelocity < 0 ) {
-				_xVelocity *= .98;
-				_yVelocity += 1.8f;
+			// increment movement values
+			_x += _speedX + _perlinWind * 10f;
+			_y += _speedY;
+			if( _speedY < 0 ) {
+				_speedX *= .98;
+				_speedY += 1.8f;
 			} else {
-				if( _yVelocity < _maxYVelocity ) _yVelocity += .1f;
-				_xVelocity *= .9;
+				if( _speedY < _maxSpeedY ) _speedY += .1f;
+				_speedX *= .9;
 			}
-			_rot += _rotVelocity;
+			_rot += _speedRot;
 			
-//			_w *= .99;
-//			_h *= .99;
-//			_tintAlpha *= .999;
+			// deactivate when off screen
+			if( _y > p.height + 30 ) {
+				_isActive = false;
+			}
+			return true;
 		}
 	}
 	
