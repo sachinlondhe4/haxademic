e34fc1640feac58d643da72d6a1c588984d21290
cacheflowe <cacheflowe@cacheflowe.com>
2012-11-17 20:27:07 -0700

    Add TColorBlendBetween for easy 2-color interp.

---
+++ src/com/haxademic/core/draw/color/TColorBlendBetween.java
@@ -0,0 +1,30 @@
+package com.haxademic.core.draw.color;
+
+import toxi.color.TColor;
+
+public class TColorBlendBetween {
+	
+	public TColor _startColor;
+	public TColor _endColor;
+	public TColor _blendColor;
+
+	public TColorBlendBetween( TColor startColor, TColor endColor ) {
+		_startColor = startColor.copy();
+		_endColor = endColor.copy();
+		_blendColor = _startColor.copy();
+	}
+	
+	public void setBaseColor( TColor color ) {
+		_startColor = color.copy();
+	}
+	
+	public void lightenColor( float lighten ) {
+		_startColor.adjustRGB( lighten, lighten, lighten );
+	}
+	
+	public int argbWithPercent( float percent ) {
+		_blendColor.setARGB( _startColor.toARGB() );
+		_blendColor.blend( _endColor, percent );
+		return _blendColor.toARGB();
+	}
+}
