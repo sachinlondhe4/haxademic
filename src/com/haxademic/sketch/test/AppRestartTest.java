5abf363b2afc2d784fb496086256b93bbcf9f771
cacheflowe <cacheflowe@cacheflowe.com>
2013-06-27 11:54:47 -0600

    Add test for AppRestart

---
+++ src/com/haxademic/sketch/test/AppRestartTest.java
@@ -0,0 +1,24 @@
+package com.haxademic.sketch.test;
+
+import com.haxademic.core.app.P;
+import com.haxademic.core.app.PAppletHax;
+import com.haxademic.core.data.FloatBuffer;
+import com.haxademic.core.system.AppRestart;
+
+@SuppressWarnings("serial")
+public class AppRestartTest
+extends PAppletHax  
+{
+	protected FloatBuffer buff;
+	
+	public void setup() {
+		super.setup();		
+		buff = new FloatBuffer(10);
+	}
+
+	public void drawApp() {
+		P.println(p.frameCount);
+		if(p.frameCount == 90) AppRestart.restart( p );
+		// TODO: write out restart command to a text file for diffing
+	}
+}
